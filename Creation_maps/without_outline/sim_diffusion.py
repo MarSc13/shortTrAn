63b3ba3d555a074c647cc9f1aaef74d472780824
Marie <marie.schwebs@gmx.de>
2020-06-03 16:17:23 +0200

    Creation of maps without outline
    Spyder scripts are executed by the jupyter notebook.
    Diffusion maps and directed motion maps are created. These maps  do not include the outline of the structure which marks the region of special interest.

---
+++ Creation_maps/without_outline/sim_diffusion.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 30 10:45:14 2019
+
+@author: mas32ea
+"""
+
+import numpy as np
+import tifffile as tif
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.collections import EllipseCollection
+from math import degrees
+import os
+
+
+
+def EllipsoidePlot(path,filename,resultpath,binning,N):
+    
+    #Generation of the resultfolder
+    if not os.path.exists(resultpath+'/DiffusionMaps'):
+            os.mkdir(resultpath+'/DiffusionMaps')
+            resultpath = resultpath+'/DiffusionMaps/'
+    else:
+            resultpath = resultpath+'/DiffusionMaps/'
+    
+    #Generation of folder for informations used for VSG diffusion simulation        
+    if not os.path.exists(resultpath+'/Info'):
+            os.mkdir(resultpath+'/Info')
+            os.mkdir(resultpath+'/Info/Angle')
+            os.mkdir(resultpath+'/Info/Ellipticity')
+            os.mkdir(resultpath+'/Info/DiffCoeffPx')
+    
+    diff_info=np.zeros((N+2,3)) #Nx2 array: first columns maximum, second 
+    #column average, two additional rows to seperate the calculated mean of 
+    #each column from the single cell data
+        
+    for i in range(1,N+1):
+        directory = path+str(i)+filename
+        
+        ten_xx = tif.imread(directory +'scld_ten_xx_fil.tif')
+        ten_xy = tif.imread(directory +'scld_ten_xy_fil.tif')
+        ten_yx = tif.imread(directory +'scld_ten_yx_fil.tif')
+        ten_yy = tif.imread(directory +'scld_ten_yy_fil.tif')
+        
+        #factor 1000 to convert nm2/ms into µm2/s and 
+        #factor 2 to consider 2*D
+        ten_xx = ten_xx/2000 
+        ten_xy = ten_xy/2000 
+        ten_yx = ten_yx/2000
+        ten_yy = ten_yy/2000
+        
+        
+        '''generates arrays needed for ellipsoid plot'''
+        #any array could be used to determine the shape, because every array 
+        #has the same shape
+        matrix=np.zeros((2,2))
+        eigenvalue=np.zeros((1,2))
+        eigenvec=np.zeros((2,2))
+        widths=np.zeros((ten_xx.shape[0],ten_xx.shape[1]))
+        widths_scal=np.zeros((ten_xx.shape[0],ten_xx.shape[1])) 
+        heights=np.zeros((ten_xx.shape[0],ten_xx.shape[1]))
+        heights_scal=np.zeros((ten_xx.shape[0],ten_xx.shape[1]))
+        angle=np.zeros((ten_xx.shape[0],ten_xx.shape[1]))
+        pix_max=np.zeros((ten_xx.shape[0],ten_xx.shape[1]))
+        
+        Y,X = np.mgrid[0:ten_xx.shape[0], 0:ten_xx.shape[1]]
+        XY = np.column_stack((X.ravel(), Y.ravel()))
+        
+        '''calculation of the eigenvalues and eigenvectors for each cell/pixel'''
+        for a in range(ten_xx.shape[0]): #any array could be used to determine 
+            #the shape, because every array has the same shape, ranges through 
+            #rows
+            for b in range(ten_xx.shape[1]): #ranges through columns
+                matrix[0,0]=ten_xx[a,b] #compilation of all 4 cohesive  
+                matrix[0,1]=ten_xy[a,b] #tensor directions
+                matrix[1,0]=ten_yx[a,b]
+                matrix[1,1]=ten_yy[a,b]
+                eigenvalue,eigenvec=np.linalg.eig(matrix) #calculation of the 
+                            #eigenvalue and the eigenvectors for each position
+                widths[a,b]=eigenvalue[0]
+                heights[a,b]=eigenvalue[1]
+                '''Normalization of ellipse length to approx 1'''
+                if widths[a,b] == 0 or heights[a,b] == 0:
+                    widths_scal[a,b] = 0
+                    heights[a,b] = 0
+                elif widths[a,b] > heights[a,b]: #use the larger value to normalize
+                    widths_scal[a,b]=widths[a,b]/widths[a,b] 
+                    heights_scal[a,b]=heights[a,b]/widths[a,b] 
+                else:
+                    widths_scal[a,b]=widths[a,b]/heights[a,b]
+                    heights_scal[a,b]=heights[a,b]/heights[a,b]
+                    
+                """ If eigv and eig or rotmat and rotmateig are not equal
+                check if angles contains negative angle values 
+                if this is the case evaluate rotmateig with -eigenvec"""
+                angles = np.array([[np.rad2deg(np.arccos(eigenvec[0,0])),-np.rad2deg(np.arcsin(eigenvec[0,1]))],
+                       [np.rad2deg(np.arcsin(eigenvec[1,0])),np.rad2deg(np.arccos(eigenvec[1,1]))]])
+                
+                if angles[0,1] < 0 or angles[1,0] < 0:
+                    eigenvec = -eigenvec
+                
+                angles = np.array([[np.rad2deg(np.arccos(eigenvec[0,0])),-np.rad2deg(np.arcsin(eigenvec[0,1]))],
+                       [np.rad2deg(np.arcsin(eigenvec[1,0])),np.rad2deg(np.arccos(eigenvec[1,1]))]])
+                angle[a,b] = angles[0,0]
+        
+        
+        
+        
+        '''Determination of the max value for colorcode and colorbar'''
+        for f in range(ten_xx.shape[0]): 
+            for g in range(ten_xx.shape[1]):
+                if widths[f,g] < heights[f,g]:
+                    pix_max[f,g] = heights[f,g]
+                else:
+                    pix_max[f,g] = widths[f,g]
+        
+        ellipticity = np.divide(widths,heights,out=np.zeros_like(widths), where=heights!=0)
+        
+        np.save(resultpath + '/Info/Angle/angle_cell_' + str(i) + '.npy', angle)
+        np.save(resultpath + '/Info/DiffCoeffPx/diffcoeff_px_cell_' + str(i) + '.npy', pix_max)
+        np.save(resultpath + '/Info/Ellipticity/ellipticity_cell_' + str(i) + '.npy', ellipticity)
+        
+        '''Following lines were inserted to count the batches more easily'''
+#        widths_scal[pix_max >= 0.45] = 0
+#        heights_scal[pix_max >= 0.45] = 0
+#        angle[pix_max >= 0.45] = 0
+#        pix_max[pix_max >= 0.45] = 0
+        
+        cmap = mpl.cm.cool
+        
+        fig, ax = plt.subplots()
+        ax.set_xlim(0-1, ten_xx.shape[1]+1)
+        ax.set_ylim(ten_xx.shape[0]+1, 0-1)
+        ax.set_aspect('equal')
+        ec = EllipseCollection(widths_scal, heights_scal, angle, units='x',
+                               offsets=XY, transOffset=ax.transData, 
+                               cmap=cmap, linewidths=0.05, edgecolor='black')
+        ec.set_array(pix_max.ravel())
+        ax.add_collection(ec)
+        cbar = plt.colorbar(ec)
+        cbar.set_label('µm$^2$/s')
+        plt.xticks([])
+        plt.yticks([])
+#        fig.text(0.5, 0.95, 'Cell'+str(i), ha='center', va='center',fontsize=12,
+#                 fontweight='bold')
+#        fig.show() #cause error message: non GUI backend
+        plt.savefig(resultpath+'EllipsoidePlot_Cell'+str(i), dpi=300)
+        
+        
+        maximum = pix_max.max()
+        pix_max[pix_max == 0] = np.nan
+        average = np.nanmean(pix_max)
+        median = np.nanmedian(pix_max)
+        
+        diff_info[i-1,:]=maximum, average, median
+        
+    
+    if N > 1: #does not make sense to clc mean over just one value
+        diff_info[-1,0]=np.mean(diff_info[0:-2,0])
+        diff_info[-1,1]=np.mean(diff_info[0:-2,1])
+        diff_info[-1,2]=np.mean(diff_info[0:-2,2])
+    
+    np.save(resultpath+'Diff_info_all_bin'+str(binning),diff_info)
+ 
+    
+if __name__ == "__main__":
+    N = 20 #number to be evaluated cells
+    binning = 160 #binning to x nm
+    resultpath = '/Users/marieschwebs/Desktop/TrackingVSGAtto/Trc/Analysis/191113/Gauss'
+    path='/Users/marieschwebs/Desktop/TrackingVSGAtto/Trc/Results_Par_testing/Trc'
+    filename = '/Binning_'+str(binning)+\
+    '/results_filtering/'
+    EllipsoidePlot(path,filename,resultpath,binning,N)
+
---
+++ Creation_maps/without_outline/sim_directed_motion.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 23 11:39:15 2019
+
+@author: mas32ea
+"""
+
+import numpy as np
+import tifffile as tif
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from matplotlib import colors
+from matplotlib import cm
+from math import atan2,degrees
+import os
+
+
+def directed_motion(path,filename,resultpath,binning,N):
+    
+    #Generation of the resultfolder
+    if not os.path.exists(resultpath+'/DirectedMotionMaps'):
+            os.mkdir(resultpath+'/DirectedMotionMaps')
+            resultpath = resultpath+'/DirectedMotionMaps/'
+    else:
+            resultpath = resultpath+'/DirectedMotionMaps/'
+    
+    speed_info=np.zeros((N+2,3)) #Nx2 array: first columns maximum, 
+    #second column average, two additional rows to seperate the calculated 
+    #mean of each column from the single cell data
+    
+    
+    for i in range(1,N+1):
+        '''Loads the bright field image'''
+    #    im=tif.imread('/home/mas32ea/Schreibtisch/Drift_and_Diffusion_Pad/BFimages/BF2/img_000000000_Default_000.tif')
+    #    plt.imshow(im, cmap='Greys')
+        
+        directory=path+str(i)+filename
+    
+        '''Loads the x and y components to calcualte the speed of the directed 
+        motion within an pixel'''
+        img_x = tif.imread(directory+'scld_vec_x_fil.tif')
+        img_y = tif.imread(directory+'scld_vec_y_fil.tif')
+        
+        img_l = np.sqrt(img_x**2 + img_y**2)
+        angle=np.zeros((img_x.shape[0],img_x.shape[1]))
+        angle_deg=np.zeros((img_x.shape[0],img_x.shape[1]))
+
+        
+        for a in range(img_x.shape[0]): #ranges through rows
+            for b in range(img_x.shape[1]): #ranges through columns
+                angle_degpar = degrees(atan2(img_y[a,b],img_x[a,b]))
+                angle_deg[a,b]=angle_degpar
+                
+                if angle_degpar <0:
+                    angle_degpar=360+angle_degpar
+                angle_deg[a,b]=angle_degpar
+                
+                if angle_degpar <=45 and angle_degpar>0 or angle_degpar>315:
+                    angle[a,b]=1
+                elif angle_degpar>45 and angle_degpar<=135:
+                    angle[a,b]=2
+                elif angle_degpar>135 and angle_degpar<=225:
+                    angle[a,b]=3
+                elif angle_degpar>225 and angle_degpar<=315:
+                    angle[a,b]=4
+                    
+
+        
+        '''Generation for the grid of the whole picture. 0/0 is in the upper 
+        left corner'''
+        Y,X = np.mgrid[0:img_x.shape[0], 0:img_y.shape[1]]
+        
+        
+        '''Genration of the black white colormap used by the quiver plot'''
+        binary = cm.get_cmap('binary', 256)
+        wb = binary(np.linspace(0, 1, 256))
+        black = np.array([0, 0, 0, 1])
+        wb[15:256, :] = black
+        wbmap = ListedColormap(wb)
+        
+        fourcmap = colors.ListedColormap(['white', 'darkorange', 'b', 'darkcyan','m'])
+        bounds=[0,1,2,3,4,5]
+        norm = colors.BoundaryNorm(bounds, fourcmap.N)
+        
+        
+        '''To display the correct orientation of incomming speed - X, Y, 
+        img_x, img_y - have to  be placed in exactly this order wihtin the 
+        quiver function. This was checked on 24.09.18 MS'''
+        fig1 = plt.figure()
+        plt.quiver(X, Y, img_x, img_y, angle, headwidth=5, linewidth=1.2, 
+                   minshaft=2.0, scale_units='height', scale=250, pivot='mid',
+                   alpha=.5, cmap=fourcmap) 
+#        plt.quiver(X, Y, img_x, img_y, img_l, headwidth=5, linewidth=1.0, 
+#                   minshaft=2.0, scale_units='height', scale=250, pivot='mid',
+#                   alpha=.5, cmap=wbmap) 
+#        plt.axis('equal')
+        plt.xlim(0-1, img_x.shape[1]+1)
+        plt.ylim(img_y.shape[0]+1, 0-1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(img_l, cmap='binary', alpha=.01)
+        tif.imsave(resultpath+'Speed_Cell'+str(i), img_l)
+        plt.savefig(resultpath+'DirectionSpeed_Cell'+str(i), dpi=300)   
+        
+        '''Generation of an speed heat map with the final unit of mum/s'''       
+        fig2 = plt.figure()
+#        plt.axis('equal')
+        plt.imshow(img_l,cmap='RdPu')
+        plt.xticks([])
+        plt.yticks([])
+#        plt.show()
+        cbar=plt.colorbar()
+        cbar.set_label('µm/s')
+        plt.savefig(resultpath+'HeatMap_Cell'+str(i), dpi=300)
+        
+        
+        maximum = img_l.max()
+        img_l[img_l == 0] = np.nan
+        average = np.nanmean(img_l)
+        median = np.nanmedian(img_l)
+        
+        speed_info[i-1,:]=maximum, average, median
+   
+    if N > 1: #does not make sense to clc mean over just one value
+        speed_info[-1,0] = np.mean(speed_info[0:-2,0])
+        speed_info[-1,1] = np.mean(speed_info[0:-2,1])
+        speed_info[-1,2] = np.mean(speed_info[0:-2,2])
+    
+    np.save(resultpath+'Speed_info_all_bin'+str(binning),speed_info)
+
+if __name__ == "__main__":
+    N = 20 #number to be evaluated cells
+    binning = 160 #binning to x nm
+    resultpath = '/Users/marieschwebs/Desktop/TrackingVSGAtto/Trc/Analysis_maps/Maps_160'
+    path='/Users/marieschwebs/Desktop/TrackingVSGAtto/Trc/Results_Par_testing/Trc'
+    filename = '/Binning_'+str(binning)+\
+    '/results_filtering/'
+    #directory = '/home/mas32ea/Schreibtisch/Drift_and_Diffusion_Pad/TrackingVSGAtto/Trc/Trc_thresh2/Trc'+str(N)+'/'
+    directed_motion(path,filename,resultpath,binning,N)
